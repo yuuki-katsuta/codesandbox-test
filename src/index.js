bcc9a3d0739311d06a27c4aa3e72d03f62117c1e
yuuki-katsuta <reptpr0314@gmail.com>
2020-11-07 19:58:52 +0900

    Add: タスク削除機能追加

--- src/index.js
+++ src/index.js
@@ -28,7 +28,11 @@ const onClickAdd = () => {
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert();
+    //押された削除ボタンの親divタグをリストから削除したい
+    //削除する要素を取得
+    const deleteTarget = deleteButton.parentNode;
+    //親から子を削除＝removeChild()
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
   });
 
   //divタグの子要素に各要素を設定 appendChild()
