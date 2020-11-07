765d459c7430f474c82050221f4b60592ff07929
yuuki-katsuta <reptpr0314@gmail.com>
2020-11-07 12:03:32 +0900

    learn js syntax

--- src/index.js
+++ src/index.js
@@ -99,24 +99,24 @@
 // const message2 = `名前は${name}、年齢は${age}です`;
 // console.log(message2);
 
-const myProfile = ["yuuki", 22];
+// const myProfile = ["yuuki", 22];
 
-const message3 = `なまえは${myProfile[0]},年齢は${myProfile[1]}です`;
-console.log(message3);
+// const message3 = `なまえは${myProfile[0]},年齢は${myProfile[1]}です`;
+// console.log(message3);
 
-//分割代入を使うと
-//配列は配列で受け取り、順番に値が渡ってくる
-const [name, age] = myProfile;
-const message4 = `名前は${name}。年齢は${age}`;
-console.log(message4);
+// //分割代入を使うと
+// //配列は配列で受け取り、順番に値が渡ってくる
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}。年齢は${age}`;
+// console.log(message4);
 
-/**
- * デフォルト値
- */
-const sayhello = (name = "ゲスト") => {
-  console.log(`こんにちは${name}さん`);
-};
-sayhello();
+// /**
+//  * デフォルト値
+//  */
+// const sayhello = (name = "ゲスト") => {
+//   console.log(`こんにちは${name}さん`);
+// };
+// sayhello();
 
 // /**
 //  * スプレッド構文
@@ -131,34 +131,34 @@ sayhello();
 // sumFunc(...arr1); //3
 
 //まとめる
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3); //[3, 4, 5] 残りの配列を受け取れる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3); //[3, 4, 5] 残りの配列を受け取れる
 
-//コピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
-const arr6 = [...arr4];
-//arr4を展開して配列にした
-console.log(arr6);
+// //コピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+// const arr6 = [...arr4];
+// //arr4を展開して配列にした
+// console.log(arr6);
 
-const arr7 = [...arr4, ...arr5];
-console.log(arr7); //[10, 20, 30, 40] 配列の結合
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7); //[10, 20, 30, 40] 配列の結合
 
-// イコールでコピーはだめ
-const arr8 = arr4;
-arr8[0] = 100;
-console.log(arr8); //[100, 20]
-console.log(arr4); //[100, 20]
+// // イコールでコピーはだめ
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8); //[100, 20]
+// console.log(arr4); //[100, 20]
 //参照が保持されるので、参照元にも影響が出る
 
 /**
  * mapとfilterを使った配列の処理
  *
  */
-const nameArr = ["yuuki", "hanahana", "tanaka"];
+//const nameArr = ["yuuki", "hanahana", "tanaka"];
 // for (let index = 0; index < nameArr.length; index++) {
 //   console.log(nameArr[index]);
 // }
@@ -182,11 +182,61 @@ const nameArr = ["yuuki", "hanahana", "tanaka"];
 // //新しい配列を返す
 // console.log(newNumArr); //[1, 3, 5]
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "yuuki") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "yuuki") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+// /**
+//  * 三項演算子 ... if elseのような記述をかんたんに書ける
+//  */
+// //ある条件 ? 条件がtrueのとき　：　条件がfalseのとき
+// const val1 = 1 > 0 ? "trueだよ" : "falseだよ";
+// console.log(val1); //trueだよ
+
+// const num = 1300;
+// //toLocaleString()は数値に対して金額表示のようなカンマ区切りで表せる
+// console.log(num.toLocaleString()); //1,300
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// //typeofで型を調べることができる
+
+// console.log(formattedNum); //1,300
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えてるよ" : "許容範囲だよ";
+// };
+
+// console.log(checkSum(50, 1));
+// //許容範囲だよ
+
+/**
+ * 論理演算子のほんとうの意味
+ */
+// const flag1 = false;
+// const flag2 = true;
+
+// //一般的な使い方
+// if (flag1 || flag2) {
+//   console.log("１か２はtrueになります");
+//   // ||は、「または」の意味
+// }
+
+// if (flag1 && flag2) {
+//   console.log("１か２はtrueになります");
+//   // ||は、「または」の意味
+// }
+
+// || は左側がfalseなら右側を返す
+const num = 100;
+const fee = num || "金額未設定";
+console.log(fee); //100
+
+//  && は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "なにか設定されました";
+console.log(fee2); //なにか設定されました
